a5ae152a31bf49b3d9b53cf0db8bb3c7cea5456c
Ravi <raviak47rv3kr@gmail.com>
2021-03-08 17:33:03 +0530

    My first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Food from './foodApp';
+import RegisterScreen from './loginscreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={RegisterScreen} />
+        <Stack.Screen name="food" component={Food} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
